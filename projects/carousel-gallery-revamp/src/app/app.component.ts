07af58da26cbc8d31fc62acf39f9e13b13a80847
SWu <SWu@nbme.org>
2025-06-05 01:01:37 -0400

    feat(carousel-gallery-revamp): implement carousel functionality with animations and service integration using the angular way and rxjs

---
+++ projects/carousel-gallery-revamp/src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { CarouselService, CarouselState } from './carousel.service';
+import { Observable, Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [CommonModule],
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  carousel$: Observable<CarouselState> = this.carouselService.state$;
+  animationDirection: 'left' | 'right' | 'none' = 'none';
+  transitioning = false;
+  autoAdvanceSub?: Subscription;
+
+  constructor(private carouselService: CarouselService) {}
+
+  ngOnInit(): void {
+    this.startAuto();
+
+    this.autoAdvanceSub = this.carouselService.autoAdvanceCommand$.subscribe(
+      (direction) => {
+        this.onCarouselButtonClick(direction);
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.carouselService.stopAutoAdvance();
+    this.autoAdvanceSub?.unsubscribe();
+  }
+
+  onCarouselButtonClick(direction: 'left' | 'right') {
+    this.startAuto();
+    if (this.transitioning) return;
+
+    this.animationDirection = direction;
+    this.transitioning = true;
+    this.carouselService.moveToNextImage(direction);
+  }
+
+  onAnimationEnd() {
+    this.animationDirection = 'none';
+    this.transitioning = false;
+  }
+
+  startAuto() {
+    this.carouselService.startAutoAdvance(4000);
+  }
+}
